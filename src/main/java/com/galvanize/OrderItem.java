e58b28bc3606a3b98ddde97d16832ab2d5c34b11
Craig Kelleher <kelleher.craig@gmail.com>
2023-01-31 14:09:40 -0800

    Implement OrderItem Class for others to inherit form

--- src/main/java/com/galvanize/OrderItem.java
+++ src/main/java/com/galvanize/OrderItem.java
@@ -1,4 +1,21 @@
 package com.galvanize;
 
+import java.math.BigDecimal;
+
 public class OrderItem {
+    private BigDecimal pricePerMonth;
+    private int numberOfMonths;
+
+    public OrderItem(BigDecimal pricePerMonth, int numberOfMonths) {
+        this.pricePerMonth = pricePerMonth;
+        this.numberOfMonths = numberOfMonths;
+    }
+
+    public BigDecimal getPricePerMonth() {
+        return pricePerMonth;
+    }
+
+    public int getNumberOfMonths() {
+        return numberOfMonths;
+    }
 }
