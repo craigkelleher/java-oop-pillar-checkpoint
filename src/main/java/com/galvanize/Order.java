ced47ee6aa165fa6e0a563f6620e6c1d4c2eca4c
Craig Kelleher <kelleher.craig@gmail.com>
2023-02-01 09:32:27 -0800

    refactor Order to no longer calculate costs of lease, purchase, and rental, but call on the abstract totalPrice method from OrderItem

--- src/main/java/com/galvanize/Order.java
+++ src/main/java/com/galvanize/Order.java
@@ -10,20 +10,9 @@ public class Order {
     private ArrayList<Object> items = new ArrayList<>();
     private BigDecimal total = new BigDecimal("0.00");
 
-    void addItem(Object item) {
+    void addItem(OrderItem item) {
         items.add(item);
-        if (item instanceof Lease) {
-            Lease lease = (Lease) item;
-            total = total.add(lease.getPricePerMonth().multiply(BigDecimal.valueOf(lease.getNumberOfMonths())));
-        }
-        else if (item instanceof Purchase) {
-            total = total.add(((Purchase) item).getPrice());
-        }
-        else if (item instanceof Rental) {
-            Rental rental = (Rental) item;
-            long days = LocalDateTime.now().until(rental.getEndDate(), ChronoUnit.DAYS) + 1;
-            total = total.add(rental.getRentalPricePerDay().multiply(BigDecimal.valueOf(days)));
-        }
+        total = total.add(item.totalPrice());
     }
 
     public BigDecimal getTotal() {
